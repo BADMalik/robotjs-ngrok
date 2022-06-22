43827137ccd03abc2b99dd2e75ddbdeb94e96b59
BADMalik <malikzaman133@gmail.com>
2022-06-22 20:48:12 +0500

    finished setup

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var robot = require("robotjs");
+const path = require('path');
 
-var indexRouter = require("./routes/index");
+const express = require('express')
+const app = express();
+var cors = require('cors')
+app.use(cors({origin: '*'}));
+// Speed up the mouse.
+// var http = require('http');
 
-var app = express();
+// http.createServer(function (req, res) {
+// }).listen(8080);
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.get('/', (req, res) => {
+    // console.log('aa');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-module.exports = app;
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.send('aaa')
+    var twoPI = Math.PI * 2.0;   
+    var screenSize = robot.getScreenSize();
+    var height = (screenSize.height);
+    var width = screenSize.width;
+// //     var x = 10000;
+    robot.moveMouseSmooth(width/2,height,2);
+    robot.moveMouseSmooth(width/2.5,( height/2.5),2);
+    robot.mouseClick();
+    robot.moveMouseSmooth(width/1.6,( height/1.5),2);
+    // await setTimeout(() =>{}, 1000);
+    robot.mouseClick();
+    // robot.mouseClick();
+    // robot.mouseClick();
+    
+    return 0;
+    //     res.send('')
+    // res.send('hello world')
+});
+  app.listen(3000, function() {
+    console.log("server start at port 3000"); // The server object listens on port 3000
+});
+
+app.get('/index', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
